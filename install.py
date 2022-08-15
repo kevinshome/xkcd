32a28356329d120fb5b2881beb6f55e6676e61c7
kevinshome <noah.tanner7@gmail.com>
2022-08-15 05:10:51 -0400

    fix install script

--- install.py
+++ install.py
@@ -1,19 +1,26 @@
 import os
+import sys
 import subprocess
-from urllib.request import urlopen
-
-if os.getuid():
-    raise SystemExit("This script requires root to run. If you don't feel safe with that (as you should), feel free to first inspect the script at 'https://github.com/kevinshome/unixstamp', or manually install UNIXstamp yourself.")
 
 if os.name != 'posix':
     raise SystemExit("Sorry, but this script only works on Linux, MacOS, and other UNIX-like systems at the moment.")
 
-print("\033[1;37mInstalling dependency 'Pillow' via pip...")
-subprocess.Popen(["pip", "install", "-U", "Pillow"])
-print("\033[1;37mInstalling xkcd script to /usr/local/bin...")
-os.chdir("/usr/local/bin")
-res = urlopen("https://raw.githubusercontent.com/nhtnr/xkcd/main/xkcd")
-with open("/usr/local/bin/xkcd", 'wb') as f:
-    f.write(res.read())
-os.chmod("/usr/local/bin/xkcd", 0o555)
+# check to see if Pillow is installed
+pip_list_proc = subprocess.Popen("pip list 2>&1 | awk '/Pillow/' | grep . >/dev/null 2>&1", shell=True)
+if pip_list_proc.wait() != 0:
+    print("\033[1;37mInstalling dependency 'Pillow' via pip...")
+    pip_proc = subprocess.Popen(["pip", "install", "-U", "Pillow"])
+    if pip_proc.wait() != 0:
+        print("\033[1;31mAn error occurred in pip, unable to install Pillow", file=sys.stderr)
+        raise SystemExit(1)
+
+print(f"\033[1;37mInstalling xkcd script to /usr/local/bin/xkcd...")
+curl_proc = subprocess.Popen("sudo curl -fsSL https://raw.githubusercontent.com/nhtnr/xkcd/main/xkcd > /usr/local/bin/xkcd", shell=True)
+if curl_proc.wait() != 0:
+    print("\033[1;31mAn error occurred in curl, unable to install xkcd", file=sys.stderr)
+    raise SystemExit(1)
+chmod_proc = subprocess.Popen(["sudo", "chmod", "555", "/usr/local/bin/xkcd"])
+if chmod_proc.wait() != 0:
+    print("\033[1;31mAn error occurred in chmod, unable to change file permissions.\nThis can be done manually, however, by running \"sudo chmod 555 /usr/local/bin/xkcd\"", file=sys.stderr)
+    raise SystemExit(1)
 print("Xkcd has been installed successfully!\nTo test it out, run: 'xkcd latest'.")
