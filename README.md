139e1397f63f9fc4c821d414a87e5a8ab8a5b498
kevinshome <noah.tanner7@gmail.com>
2022-08-22 07:02:04 -0400

    add search option

--- README.md
+++ README.md
@@ -11,5 +11,19 @@ To install this script, run the following command:
 curl -fsSL https://raw.githubusercontent.com/nhtnr/xkcd/main/install.py | python3 -
 ```
 
+To install with the full database of comics:
+```
+curl -fsSL https://raw.githubusercontent.com/nhtnr/xkcd/main/install.py | python3 - --install-db
+```
+
+Database
+==========
+
+This repository contains a SQLite3 database with all available comic information (as of 2022-08-21).
+In order to search by keyword, this database is required. It can be installed either by running the installation script with the "--install-db" flag, or directly from PyPI (xkcd-comicdb).
+
+License
+=========
+
 This software is released into the public domain under the terms of the Unlicense, made
 available in the 'LICENSE' file in the root of this project.
--- install.py
+++ install.py
@@ -2,21 +2,41 @@ import os
 import sys
 import subprocess
 
-if os.path.exists("/usr/local/bin/xkcd"):
+requires_dist = []
+pass_xkcd_installed_checker = False
+xkcd_installed = False
+
+if sys.argv[1] == "--install-db":
+    pip_list_proc = subprocess.Popen("pip3 list 2>&1 | awk '/xkcd-comicdb/' | grep . >/dev/null 2>&1", shell=True)
+    if pip_list_proc.wait() != 0:
+        requires_dist.append("xkcd-comicdb")
+        pass_xkcd_installed_checker = True
+    else:
+        print("'xkcd-comicdb' already installed. Ignoring '--install-db' flag...")
+
+if os.path.exists("/usr/local/bin/xkcd") and not pass_xkcd_installed_checker:
     print("\033[1;31mxkcd already installed. Aborting...", file=sys.stderr)
     raise SystemExit(1)
+elif os.path.exists("/usr/local/bin/xkcd") and pass_xkcd_installed_checker:
+    xkcd_installed = True
 
 if os.name != 'posix':
     raise SystemExit("Sorry, but this script only works on Linux, MacOS, and other UNIX-like systems at the moment.")
 
 # check to see if Pillow is installed
-pip_list_proc = subprocess.Popen("pip list 2>&1 | awk '/Pillow/' | grep . >/dev/null 2>&1", shell=True)
+pip_list_proc = subprocess.Popen("pip3 list 2>&1 | awk '/Pillow/' | grep . >/dev/null 2>&1", shell=True)
 if pip_list_proc.wait() != 0:
-    print("\033[1;37mInstalling dependency 'Pillow' via pip...")
-    pip_proc = subprocess.Popen(["pip", "install", "-U", "Pillow"])
+    requires_dist.append("Pillow")
+
+for package in requires_dist:
+    print(f"\033[1;37mInstalling dependency '{package}' via pip...")
+    pip_proc = subprocess.Popen(["pip3", "install", "-U", package])
     if pip_proc.wait() != 0:
-        print("\033[1;31mAn error occurred in pip, unable to install Pillow", file=sys.stderr)
+        print(f"\033[1;31mAn error occurred in pip, unable to install {package}", file=sys.stderr)
         raise SystemExit(1)
+
+if xkcd_installed:
+    raise SystemExit(0)
 
 print(f"\033[1;37mInstalling xkcd script to /usr/local/bin/xkcd...")
 curl_proc = subprocess.Popen("sudo curl -fsSL https://raw.githubusercontent.com/nhtnr/xkcd/main/xkcd > /usr/local/bin/xkcd", shell=True)
